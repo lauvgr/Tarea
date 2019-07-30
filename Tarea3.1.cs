9d4cec19a946e2f7755519cd0be62d85ab5ad6bb
lauvgr <53203564+lauvgr@users.noreply.github.com>
2019-07-29 23:27:26 -0500

    Tarea 3

---
+++ Tarea3.1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su salario: ");
+            double sal = double.Parse(Console.ReadLine());
+            double smmlv = 828116;
+
+            if (sal < (smmlv * 2))
+            {
+                Console.WriteLine("Le corresponde la tarifa A");
+
+            }
+            else if ((smmlv * 2) <= sal && sal < (smmlv * 4))
+
+            {
+                Console.WriteLine("Le corresponde la tarifa B");
+
+            }
+            else if (sal > (smmlv * 4))
+            {
+                Console.WriteLine("Le corresponde la tarifa C");
+
+            }
+        }
+    }
+}
---
+++ Tarea3.2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+         
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su salario: ");
+            double sal = double.Parse(Console.ReadLine());
+            double smlmv = 828116;
+            double smlmdv = (sal / 30);
+
+            if (sal < (smlmv * 2))
+            {
+                double a = smlmdv * 0.1170;
+                Console.WriteLine("Le corresponde la tarifa A");
+                Console.WriteLine(a);
+
+            }
+            else if ((smlmv * 2) <= sal && sal < (smlmv * 5))
+
+            {
+                Console.WriteLine("Le corresponde la tarifa B");
+                double b = smlmdv * 0.4610;
+                Console.WriteLine(b);
+
+
+            }
+            else if (sal > (smlmv * 5))
+            {
+                Console.WriteLine("Le corresponde la tarifa C");
+                double c = smlmdv * 1.2150;
+                Console.WriteLine(c);
+
+            }
+        }
+    }
+}
+
