e56845a89f53b1d7d3747641a725d36c6353e82b
lauvgr <53203564+lauvgr@users.noreply.github.com>
2019-10-09 23:30:55 -0500

    00_Proyecto_tanque

---
+++ Construcción.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Construcción : MonoBehaviour
+{
+    
+    [SerializeField] float salud = 100;
+    [SerializeField] MotorLau tanque;
+
+    public void Damage()
+    {
+        salud -= tanque.damage / 2;
+    }
+}
